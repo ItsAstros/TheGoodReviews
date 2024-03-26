5dfa05a2f212c42c839688531d528227725ae6f4
astros <pierre.bertholom@gmail.com>
2024-03-26 07:46:05 +0100

    Ajout des sections public_dash

--- src/dashboards/public_dashboard.php
+++ src/dashboards/public_dashboard.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="style_public.css">
 </head>
 <body>
-    <div class="container">
+    <div class="container-loader">
       <div class="pre-loader">
         <div class="loader"></div>
         <div class="loader-bg"></div>
@@ -29,21 +29,106 @@
     </nav>
     <div class="site-content">
       <div class="header">
-        <h1>T</h1>
-        <h1>h</h1>
-        <h1>e</h1>
-        <h1>G</h1>
-        <h1>o</h1>
-        <h1>o</h1>
-        <h1>d</h1>
-        <h1>R</h1>
-        <h1>e</h1>
-        <h1>v</h1>
-        <h1>i</h1>
-        <h1>e</h1>
-        <h1>w</h1>
-        <h1>s</h1>
+        <div class="title">
+          TheGoodReviews
+        </div>
       </div>
+      <section class="section-one">
+        <div class="container">
+          <video class="background-video" autoplay loop muted>
+            <source src="../../media/backgroundvid.mp4" type="video/mp4">
+          </video>
+            <div class="inverted-h1">
+              <h1>Fueling the Gaming Conversation.</h1>
+            </div>
+        </div>
+      </section>
+
+      <section class="section-two">
+        <div class="zoom-content">
+          <div class="left-side">
+            <img class="img-2" src="../../media/footer1.jpeg">
+          </div>
+          <div class="right-side">
+            <div class="title">
+              <h1>See the latest news</h1>
+              <h2>Learn about your favorite games, last changes and updates</h2>
+              <p class="p">
+                A blabla bliblou blabla oh j'ai plus d'inspi moi c'est bon
+              </p>
+
+              <div class="line"></div>
+
+              <p class="p">
+                Alala c'est un truc de fou ce site de malade mental
+              </p>
+              <button>Clique batard</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="container">
+          <div class="left-side">
+            <div class="title">
+              <h1>A new day a new h1</h1>
+              <h2>Smoothie à la fraise</h2>
+            </div>
+            <div class="type g-type">
+              <p>Jean</p>
+              <p>Celeste est le meilleur jeu de plateforme</p>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="type s-type">
+              <p>Ouioui</p>
+              <p>Pokemon c'est marrant</p>
+            </div>
+          </div>
+          <div class="right-side">
+            <img class="img-3" src="../../media/footer2.jpeg">
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="container">
+          <div class="left-side">
+            <div class="title">
+              <h1>Select a game here</h1>
+              <h2>Choose or consequences</h2>
+            </div>
+          </div>
+          <div class="right-side">
+            <p>
+              Bon bah voila on ecrit mon gars tu connais j'ai tjs par d'inspi
+            </p>
+
+            <div class="line"></div>
+          </div>
+        </div>
+
+        <div class="card-container">
+          <div class="card card-1">
+            <img src="../../media/card-1.jpg" class="img-card">
+            <div class="color">yellow</div>
+            <span>585 standard gold</span>
+          </div>
+          <div class="card card-2">
+            <img src="../../media/card-2.jpg" class="img-card">
+            <div class="color">white</div>
+            <span>585 standard gold</span>
+          </div>
+          <div class="card card-3">
+            <img src="../../media/card-3.jpg" class="img-card">
+            <div class="color">pink</div>
+            <span>750 standard gold</span>
+          </div>
+        </div>
+      </section>
+
       <footer>
         <div class="footer-copy">
           <p>
@@ -53,108 +138,15 @@
           </p>
         </div>
         <div class="footer-nav">
-          <div class="img"></div>
-          <div class="img"></div>
+          <img src="../../media/footer1.jpeg" class="img">
+          <img src="../../media/footer2.jpeg"class="img">
         </div>
       </footer>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.2/anime.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
-    <script>
-        function startLoader() {
-            let counterElement = document.querySelector(".count p");
-            let currentValue = 0;
-            
-            function updateCounter() {
-                if(currentValue < 100){
-                    let increment = Math.floor(Math.random() * 10) + 1;
-                    currentValue = Math.min(currentValue + increment, 100);
-                    counterElement.textContent = currentValue;
-
-                    let delay = Math.floor(Math.random() * 200) + 25;
-                    setTimeout(updateCounter, delay);
-                }
-            }
-
-            updateCounter();
-        }
-        startLoader();
-        gsap.to(".count", { opacity: 0, delay: 3.5, duration: 0.5});
-
-        let textWrapper = document.querySelector(".ml16");
-        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-        anime.timeline({ loop: false})
-            .add({
-                targets: '.ml16 .letter',
-                translateY:  [-100, 0],
-                easing: "easeOutExpo",
-                duration: 1500,
-                delay: (el, i) => 30*i
-            })
-            .add({
-                targets: '.ml16 .letter',
-                translateY:  [0, 100],
-                easing: "easeOutExpo",
-                duration: 3000,
-                delay: (el, i) => 2000 + 30*i
-            });
-
-            gsap.to(".pre-loader", {
-                scale: 0.5,
-                ease: "power4.inOut",
-                duration: 2,
-                delay: 3,
-                onComplete: function() {
-                  document.querySelector('.glassmorphism-nav').style.opacity = '0';
-                  document.querySelector('.glassmorphism-nav').style.display = 'flex';
-
-                
-                gsap.to(".glassmorphism-nav", {
-                    opacity: 1,
-                    duration: 1,
-                    ease: "power1.inOut"
-                }); 
-                }
-            })
-
-            gsap.to(".loader", {
-                height: "0",
-                ease: "power4.inOut",
-                duration: 1.5,
-                delay: 3.75
-            })
-
-            gsap.to(".loader-bg", {
-                height: "0",
-                ease: "power4.inOut",
-                duration: 1.5,
-                delay: 4
-            })
-
-            gsap.to(".loader-2", {
-                clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-                ease: "power4.inOut",
-                duration: 1.5,
-                delay: 3.5
-            })
-
-            gsap.from(".header h1", {
-                y: 200,
-                ease: "power4.inOut",
-                duration: 1,
-                delay: 3.75,
-                stagger: 0.05
-            })
-
-            gsap.to(".img", {
-                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-                ease: "power4.inOut",
-                duration: 1.5,
-                delay: 4.5,
-                stagger: 0.25
-            })
-
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
+    <script src="../js/loading.js"></script>
+    <script src="../js/content.js"></script>
 </body>
 </html>
