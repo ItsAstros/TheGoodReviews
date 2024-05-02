dbfa775557a5407106c3e96239e9646d6a4b3ed3
Florian Cliquet <florian.cliquet.pro@gmail.com>
2024-05-02 13:10:38 +0200

    Pierre t chelou

--- src/user/profil.php
+++ src/user/profil.php
@@ -123,7 +123,7 @@ if ($reviewCount > 44) {
 <html>
 <head>
     <title>TheGoodReviews - User Profile</title>
-    <link rel="stylesheet" href="profil.css">
+    <link rel="stylesheet" href="../../assets/css/profil-style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
@@ -389,7 +389,9 @@ fileInput.addEventListener('change', function() {
             method: 'POST',
             body: formData
         })
-        .then(response => response.json())
+        .then(response => response)
+
+        .then(response => console.log(response))
         .then(data => {
             if (data.success) {
                 const imagePath = '../../assets/images/pp/' + data.fileName;
